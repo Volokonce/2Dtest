c384b16801c578b2ef00da589904acd15d7ae5f4
Volokonce <a.volokotsev@volokonce.ru>
2020-06-08 20:00:10 +0300

    Initial commit

---
+++ Assets/Scripts/Game/Ham/CoinSpawn.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinSpawn : MonoBehaviour
+{
+    [SerializeField] private GameObject _coin;
+    [SerializeField] private int _spawnTimer;
+
+    private Transform _spawn;
+    private Transform[] _spawnPoints;
+        
+    private void Awake()
+    {
+        _spawn = GetComponent<Transform>();
+        _spawnPoints = GetComponentsInChildren<Transform>();
+       
+    }
+
+    private void Start()
+    {
+        StartCoroutine(CreateHam());       
+    }
+
+    private IEnumerator CreateHam()
+    {
+        var waitSecods = new WaitForSeconds(_spawnTimer);
+        for (int i = 1; i < _spawnPoints.Length; i++)
+        {
+            Instantiate(_coin, _spawnPoints[i]);
+            
+            yield return waitSecods;
+        }
+
+    }
+}
